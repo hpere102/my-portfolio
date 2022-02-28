02c84205bba13e7a5a7b09cac8ee416baaa40f7b
danny p <danny@dannys-MacBook-Pro.local>
2022-02-28 18:40:54 -0500

    css added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,13 +5,13 @@ const Header = () => {
 
   return (
     
-    <header className="">
+    <header className="header-cont">
       
-        <nav className="nav-links">
-              <Link to="/">home</Link>
-              <Link to="/skills">skills</Link>    
-              <Link to ="/projects">projects</Link>
-              <Link to="/about">about me</Link>
+        <nav className="nav-link-cont">
+              <Link className="nav-link" to="/">home</Link>
+              <Link className="nav-link" to="/skills">skills</Link>    
+              <Link className="nav-link" to ="/projects">projects</Link>
+              <Link className="nav-link" to="/about">about me</Link>
         </nav>
       
     </header>
